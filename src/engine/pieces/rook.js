d1a0702c6cd41a72a9b05a1783203c9ea745e5d0
Nick Osman <Nick.Osman@softwire.com>
2022-08-16 14:16:51 +0100

    rook latteral moves

--- src/engine/pieces/rook.js
+++ src/engine/pieces/rook.js
@@ -1,3 +1,4 @@
+import Square from '../square';
 import Piece from './piece';
 
 export default class Rook extends Piece {
@@ -6,6 +7,19 @@ export default class Rook extends Piece {
     }
 
     getAvailableMoves(board) {
-        return new Array(0);
+
+        let location = board.findPiece(this)
+
+        let moves = []
+ 
+        for (let i = 0; i<8; i++){
+            if (i !=location.row){
+                moves.push(Square.at(i, location.col))
+            }
+            if (i != location.col){
+                moves.push(Square.at(location.row, i))
+            }
+        }
+        return moves;
     }
 }
