07c86ce9896ca183afc528ec41bcb6bb8708abc8
tomscambler <tomscambler@gmail.com>
2022-08-17 11:54:02 +0100

    refactoring rook like a boss

--- src/engine/pieces/rook.js
+++ src/engine/pieces/rook.js
@@ -15,65 +15,32 @@ export default class Rook extends Piece {
         let pieceToCheck;
 
         let moves = [];
- 
-        // for (let i = 0; i<8; i++){
-        //     if (i !=location.row){
-        //         moves.push(Square.at(i, location.col))
-        //     }
-        //     if (i != location.col){
-        //         moves.push(Square.at(location.row, i))
-        //     }
-        // }
 
-        //check up
         [-1,+1].forEach(direction => {
-            for ( let i=row+direction; i>=0 && i<=7; i+=direction ){
-                squareToCheck = Square.at(i, col);
-                pieceToCheck = board.getPiece(squareToCheck);
-                if (!pieceToCheck){
-                    moves.push(squareToCheck);
+
+            [{fixed : row, moving: col},
+             {fixed : col, moving: row}].forEach( axis => {
+
+                for ( let i = axis.moving + direction; i>=0 && i<=7; i+=direction ){
+
+                    let [x, y] = axis.moving===row ? [i, axis.fixed] : [axis.fixed, i];
+
+                    squareToCheck = Square.at(x, y);
+                    pieceToCheck = board.getPiece(squareToCheck);
+
+                    if (!pieceToCheck){
+                        moves.push(squareToCheck);
+                    }
+                    else if (pieceToCheck.player !== this.player){
+                        moves.push(squareToCheck);
+                        break;
+                    }
+                    else {
+                        break;
+                    }
                 }
-                else if (pieceToCheck.player !== this.player){
-                    moves.push(squareToCheck);
-                    break;
-                }
-                else {
-                    break;
-                }
-            }
+            });
         });
-
-        //check right
-        for ( let j=col+1; j<=7; j++ ){
-            squareToCheck = Square.at(row, j);
-            pieceToCheck = board.getPiece(squareToCheck);
-            if (!pieceToCheck){
-                moves.push(squareToCheck);
-            }
-            else if (pieceToCheck.player !== this.player){
-                moves.push(squareToCheck);
-                break;
-            }
-            else {
-                break;
-            }
-        }
-        //check left
-        for ( let j=col-1; j>=0; j--){
-            squareToCheck = Square.at(row, j);
-            pieceToCheck = board.getPiece(squareToCheck);
-            if (!pieceToCheck){
-                moves.push(squareToCheck);
-            }
-            else if (pieceToCheck.player !== this.player){
-                moves.push(squareToCheck);
-                break;
-            }
-            else {
-                break;
-            }
-        }
-
 
         return moves;
     }
