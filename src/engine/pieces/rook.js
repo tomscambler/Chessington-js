404a4b813d36523341b483e235644509572ddade
tomscambler <tomscambler@gmail.com>
2022-08-17 13:32:47 +0100

    rook checking w.i.p.

--- src/engine/pieces/rook.js
+++ src/engine/pieces/rook.js
@@ -1,3 +1,4 @@
+import Player from '../player';
 import Square from '../square';
 import Piece from './piece';
 
@@ -16,23 +17,38 @@ export default class Rook extends Piece {
 
         let moves = [];
 
+        //console.log(board.getPiece(Square.at(4,6)),"###########################################################");
+
+        // for (let i=0; i<7; i++){
+        //     for (let j=0; j<7; j++){
+        //         let piece = board.getPiece(Square.at(i,j));
+        //         console.log(piece);
+        //     }
+        // }
+
+
         [-1,+1].forEach(direction => {
 
             [{fixed : row, moving: col},
              {fixed : col, moving: row}].forEach( axis => {
+                console.log("===============================================");
+                console.log(row, col);
 
                 for ( let i = axis.moving + direction; i>=0 && i<=7; i+=direction ){
 
                     let [x, y] = axis.moving===row ? [i, axis.fixed] : [axis.fixed, i];
 
-                    squareToCheck = Square.at(x, y);
+                    squareToCheck = Square.at(4, 6);
                     pieceToCheck = board.getPiece(squareToCheck);
-
+                    
+                    console.log(pieceToCheck,x,y);
                     if (!pieceToCheck){
                         moves.push(squareToCheck);
+                        //console.log(x,y,pieceToCheck,moves);
                     }
-                    else if (pieceToCheck.player !== this.player){
+                    else if (pieceToCheck && pieceToCheck.player !== this.player){
                         moves.push(squareToCheck);
+                        //console.log(x,y,pieceToCheck,moves);
                         break;
                     }
                     else {
@@ -43,5 +59,5 @@ export default class Rook extends Piece {
         });
 
         return moves;
-    }
+     }
 }
