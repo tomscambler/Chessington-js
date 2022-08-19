f4cdd8b7671fae17c9effc5c740586df50abde4a
tomscambler <tomscambler@gmail.com>
2022-08-19 11:21:53 +0100

    red-12 passing

--- src/engine/pieces/king.js
+++ src/engine/pieces/king.js
@@ -1,5 +1,6 @@
 import Piece from './piece';
 import Board from '../../../src/engine/board';
+import Square from '../square';
 
 export default class King extends Piece {
     constructor(player) {
@@ -10,15 +11,28 @@ export default class King extends Piece {
         
         let location = board.findPiece(this);
         let row = location.row;
-        let col = location.row;
+        let col = location.col;
         let moves = [];
         let squareToCheck; let pieceToCheck;
 
-        [-1, 0, 1].forEach(horizontalMove => {
-            [-1, 0, 1].forEach(verticalMove => {
-                
-            })
-        })
+        [-1, 0, +1].forEach(horizontalMove => {
+            [-1, 0, +1].forEach(verticalMove => {
+
+                squareToCheck = Square.at(row + horizontalMove, col + verticalMove);
+
+                if (board.checkSquareWithinBoard(squareToCheck)){
+
+                    //if (horizontalMove || verticalMove){
+
+                        pieceToCheck = board.getPiece(squareToCheck);
+
+                        if (!pieceToCheck || pieceToCheck.player !== this.player){
+                            moves.push(squareToCheck);
+                        }
+                    //}
+                }
+            });
+        });
 
         return moves;
     }
